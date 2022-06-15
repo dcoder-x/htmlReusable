2f544e87044cb227e0cc48397a3379794ed856f7
dcoder-x <ayomikunfaluyi@gmail.com>
2022-06-15 01:05:08 +0100

    added reusable script

---
+++ reusable.js
@@ -0,0 +1,23 @@
+
+const reuse =(componentName,component)=>{
+
+
+    class Reusable extends HTMLElement{
+        constructor(){
+            super();
+            const componentTemplate = document.createElement('template'); 
+            componentTemplate.innerHTML=component
+            const shadowRoot = this.attachShadow({ mode: 'open' });
+        
+            shadowRoot.appendChild(componentTemplate.content);
+    
+        }
+        connectedCallback() {
+          }
+    }
+    customElements.define(componentName,Reusable);
+
+    return Reusable
+}
+
+
